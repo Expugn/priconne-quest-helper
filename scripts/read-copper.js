2e93aa68f24e3f3a21cc544b38cf7ad6bb6e8e8d
Expugn <Expugn@users.noreply.github.com>
2019-01-16 19:22:00 -0800

    add copper items

--- scripts/read-copper.js
+++ scripts/read-copper.js
@@ -1,28 +1,28 @@
-function read_common()
+function read_copper()
 {
-    const totalCommonItems = 15;
+    const totalCopperItems = 38;
 
     let returned_data = [];
     let entryArray = [];
     let itemMap = new Map();
 
-    for (let i = 1 ; i < totalCommonItems + 1 ; i++)
+    for (let i = 1 ; i < totalCopperItems + 1 ; i++)
     {
         /* ROUND UP VALUE */
-        let itemValue = Math.round(document.getElementById("common-" + i + "-amt").value);
-        document.getElementById("common-" + i + "-amt").value = itemValue;
+        let itemValue = Math.round(document.getElementById("copper-" + i + "-amt").value);
+        document.getElementById("copper-" + i + "-amt").value = itemValue;
 
         if (itemValue >= 1)
         {
             /* CHECK VALUE: IF GREATER THAN 100, THEN CORRECT THE ISSUE */
             if (itemValue > 99)
             {
-                document.getElementById("common-" + i + "-amt").value = 99;
+                document.getElementById("copper-" + i + "-amt").value = 99;
                 itemValue = 99;
             }
 
             let tableEntry = "";
-            let itemName = document.getElementById("common-" + i).title;
+            let itemName = document.getElementById("copper-" + i).title;
 
             tableEntry += "<td>" + itemName + "</td>";
             tableEntry += "<td>" + itemValue + "</td>";
