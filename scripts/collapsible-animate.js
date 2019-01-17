f14f46c8bace846bbed5d99bc42f8ad65ea573f8
Expugn <Expugn@users.noreply.github.com>
2019-01-17 10:02:32 -0800

    Collapsible Item Tables and Common Item Adjustment
    - Common Item Table adjusted from 6 long to 7

---
+++ scripts/collapsible-animate.js
@@ -0,0 +1,21 @@
+function enableCollapsible()
+{
+    let coll = document.getElementsByClassName("collapsible");
+
+    for (let i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function() {
+            let content = this.nextElementSibling;
+            this.classList.toggle("active");
+
+            if (content.style.maxHeight){
+                /* HIDE CONTENT */
+                content.style.maxHeight = null;
+                content.style.overflow = "hidden";
+            } else {
+                /* SHOW CONTENT */
+                content.style.maxHeight = content.scrollHeight + "px";
+                content.style.overflow = "auto";
+            }
+        });
+    }
+}
