9acb6c497bc67f373403a48979e93218eec42279
Expugn <Expugn@users.noreply.github.com>
2019-03-20 01:56:12 -0700

    Add English Translated Names
    - I tried translating this stuff myself, so it may be incorrect. I got tired of waiting for the wiki to update their names so I might just put my own translation next time.
    - Please let me know if I got something wrong. I'm a novice at this.

--- scripts/update-progress.js
+++ scripts/update-progress.js
@@ -6,7 +6,7 @@ function update_progress()
     const normal_quest_updated = true;
     const hard_quest_updated = true;
     const equipment_selection_updated = true;
-    const english_translated_names_updated = false;
+    const english_translated_names_updated = true;
     const rank_equipment_updated = false;
 
     if (update_in_progress)
