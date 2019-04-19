f0373f0a4c480ede1ac0cd4aa2d8eae70bc77bbb
Expugn <Expugn@users.noreply.github.com>
2019-04-19 03:17:36 -0700

    4/19/2019 Quest Update (3)
    - Added Chapter 22 Normal Quest Data

--- scripts/update-progress.js
+++ scripts/update-progress.js
@@ -3,7 +3,7 @@ function update_progress()
     const update_in_progress = true;
     const update_name = "April 19, 2019";
 
-    const normal_quest_updated = false;
+    const normal_quest_updated = true;
     const hard_quest_updated = true;
     const equipment_selection_updated = true;
     const english_translated_names_updated = true;
