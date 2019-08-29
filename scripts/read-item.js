dbf0c2836688221451ead070253190d130e60200
SD SkyKlouD <sdsk@sdsk.one>
2019-08-29 16:49:45 +0900

    Clarify argument's meaning

--- scripts/read-item.js
+++ scripts/read-item.js
@@ -1,23 +1,23 @@
-function read_item(itemType)
+function read_item(itemRarity)
 {
     let itemMap = new Map();
 
-    for (let i = 1 ; i < totalItemsCount[itemType] + 1 ; i++)
+    for (let i = 1 ; i < totalItemsCount[itemRarity] + 1 ; i++)
     {
         /* ROUND UP VALUE */
-        let itemValue = Math.round(document.getElementById(itemType + "-" + i + "-amt").value);
-        document.getElementById(itemType + "-" + i + "-amt").value = itemValue;
+        let itemValue = Math.round(document.getElementById(itemRarity + "-" + i + "-amt").value);
+        document.getElementById(itemRarity + "-" + i + "-amt").value = itemValue;
 
         if (itemValue >= 1)
         {
             /* CHECK VALUE: IF GREATER THAN 100, THEN CORRECT THE ISSUE */
             if (itemValue > 99)
             {
-                document.getElementById(itemType + "-" + i + "-amt").value = 99;
+                document.getElementById(itemRarity + "-" + i + "-amt").value = 99;
                 itemValue = 99;
             }
 
-            let itemName = document.getElementById(itemType + "-" + i).title;
+            let itemName = document.getElementById(itemRarity + "-" + i).title;
             itemMap.set(itemName, itemValue);
         }
     }
